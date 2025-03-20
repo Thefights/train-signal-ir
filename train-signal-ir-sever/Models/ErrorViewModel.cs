08d1aeb810c71027c2e192739a3941d96fc60d4d
QuachKhang <phuckhang1088@gmail.com>
2025-03-20 11:14:46 +0700

    create train-signal-ir-sever folder

---
+++ train-signal-ir-sever/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace train_signal_ir_sever.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
